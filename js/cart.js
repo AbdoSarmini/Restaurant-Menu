66b7d8d3928b56a599da3790111ace6d03460a31
AbdoSarmini <abdosarmini92@gmail.com>
2024-12-22 22:57:32 +0300

    cart quantity function

--- js/cart.js
+++ js/cart.js
@@ -4,10 +4,23 @@ loadCartItemsFromStorage();
 renderCartItems();
 
 function onAddToCart(id) {
-  cart.push(menuItems.find((item) => item.id == id));
-  localStorage.setItem("cartItems", JSON.stringify(cart));
+  var itemToAdd = menuItems.find((item) => item.id == id);
+  itemToAdd.count = 1;
 
-  console.log(cart);
+  // Find item count
+  var indexOfOldItem = cart.findIndex(
+    (cartItem) => cartItem.id == itemToAdd.id
+  );
+  if (indexOfOldItem != -1) {
+    console.log(indexOfOldItem);
+
+    cart[indexOfOldItem].count = parseInt(cart[indexOfOldItem].count) + 1;
+    console.log("un update" + cart[indexOfOldItem]);
+  } else {
+    cart.push(itemToAdd);
+    console.log("un new" + itemToAdd);
+  }
+  localStorage.setItem("cartItems", JSON.stringify(cart));
 }
 
 function loadCartItemsFromStorage() {
@@ -21,27 +34,64 @@ function deleteItemFromCart(index) {
   renderCartItems();
 }
 
+// function onCountChange(index) {
+//   var value = document.getElementById("countInput" + index).value;
+//   cart[index].count = value;
+//   localStorage.setItem("cartItems", JSON.stringify(cart));
+
+//   renderCartItems();
+// }
+
+function decreaseCount(index) {
+  var newCount = parseInt(cart[index].count) - 1;
+  if (newCount > 0) cart[index].count = newCount;
+
+  localStorage.setItem("cartItems", JSON.stringify(cart));
+
+  renderCartItems();
+}
+
+function increaseCount(index) {
+  cart[index].count = parseInt(cart[index].count) + 1;
+
+  localStorage.setItem("cartItems", JSON.stringify(cart));
+
+  renderCartItems();
+}
+
 function renderCartItems() {
   var cartItemsContainer =
     document.getElementsByClassName("cartItemsContainer")[0];
   if (cartItemsContainer == undefined) return;
   cartItemsContainer.innerHTML = "";
   cart.forEach((item, index) => {
+    item.totalPrice =
+      parseFloat(item.price.substring(1, item.price.length - 1)) *
+      parseInt(cart[index].count);
+
+    item.totalPrice = item.totalPrice.toFixed(2);
     cartItemsContainer.insertAdjacentHTML(
       "beforeend",
+      // <input oninput="onCountChange(${index})" name="count" id="countInput${index}" class="countInput" type="number" value="${item.count}">
 
       `
     <div class="menuItem">
           <img src="./images/placeholder.jpg" alt="" />
           <div class="menuContent">
-            <div>
+            <div style="width:70%;">
             <h2>${item.title}</h2>
             <p class="menuDesc">
               ${item.description}
             </p>
             </div>
             <div class="menuItemFooter">
-              <h3 class="price">${item.price}</h3>
+            <button class="counterButton" onclick="decreaseCount(${index})" >-</button>
+              
+              <div style="min-width:20px; text-align:center"><p> ${item.count} </p></div>
+            <button class="counterButton" onclick="increaseCount(${index})" >+</button>
+
+              <p>x ${item.price} </p>
+              <div style="min-width:110px; text-align:right; "><h3 class="price">$${item.totalPrice}</h3></div>
               <button onclick="deleteItemFromCart(${index})" class="linkButton" href="#">Delete</button>
             </div>
           </div>
